60ef463f1a8f1bfcec5f3c5a968bb54f98b937ec
salmannhr <salman.hamalarizky@student.telkomuniversity.ac.id>
2024-05-30 10:57:02 +0700

    clean code

--- modul5_1302220085/Program.cs
+++ modul5_1302220085/Program.cs
@@ -1,31 +1,33 @@
 ﻿public class SimpleDataBase<T>
 {
-    private List<T> storedData;
-    private List<DateTime> inputDates;
+    private List<T> _storedData;
+    private List<DateTime> _inputDates;
 
     public SimpleDataBase()    
     {
-        storedData = new List<T>();
-        inputDates = new List<DateTime>();
+        _storedData = new List<T>();
+        _inputDates = new List<DateTime>();
     }
 
     public void AddNewData(T data)
     {
-        storedData.Add(data);
-        inputDates.Add(DateTime.Now);
+        _storedData.Add(data);
+        _inputDates.Add(DateTime.Now);
     }
 
     public void PrintAllData()
     {
-        for (int i = 0; i < storedData.Count; i++)
+        for (int i = 0; i < _storedData.Count; i++)
         {
-            if (storedData[i] > (dynamic)0 && storedData[i] < (dynamic)10)
+            if (_storedData[i] > (dynamic)0 && _storedData[i] < (dynamic)10)
             {
-                Console.WriteLine($"Data " + i + " berisi: 0" + storedData[i] + ", yang disimpan pada waktu UTC " + inputDates[i]);
+                Console.WriteLine($"Data " + (i+1) + " berisi: 0" + _storedData[i] +
+                    ", yang disimpan pada waktu UTC " + _inputDates[i]);
             }
             else
             {
-                Console.WriteLine($"Data " + i + " berisi: " + storedData[i] + ", yang disimpan pada waktu UTC " + inputDates[i]);
+                Console.WriteLine($"Data " + (i+1) + " berisi: " + _storedData[i] + 
+                    ", yang disimpan pada waktu UTC " + _inputDates[i]);
             }
         }
     }
@@ -36,7 +38,7 @@ public class Penjumlahan
     public void JumlahTigaAngka<T>(T angka1, T angka2, T angka3) 
     {
         dynamic hasil;
-        hasil = (dynamic)angka1 + (dynamic)angka2 + (dynamic)angka3;
+        hasil = angka1 as dynamic + angka2 as dynamic + angka3 as dynamic;
         Console.WriteLine($"Penjumlahan 3 double anda yaitu {hasil}");
     }
 }
@@ -44,13 +46,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // method untuk menjumlahkan 3 angka double.
         Penjumlahan jum = new Penjumlahan();
         jum.JumlahTigaAngka<double>(1.5, 2.5, 3.5);
-
+        // method untuk menyimpan data ke database.
         SimpleDataBase<double> data = new SimpleDataBase<double>();
         data.AddNewData(13);
         data.AddNewData(02);
         data.AddNewData(22);
+        // method untuk print isi database dan waktu data tersimpan.
         data.PrintAllData();
     }
 }
